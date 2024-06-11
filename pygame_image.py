a471429c04610718b5021181f88a714b839c4de7
yujin <c0a23152cc@edu.teu.ac.jp>
2024-06-11 16:03:41 +0900

    こうかとん表示

--- pygame_image.py
+++ pygame_image.py
@@ -11,13 +11,16 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kou_img = pg.image.load("fig/3.png")
-    kou_img = pg.transform.bool(kou_img,True, False)
+    kou_img = pg.transform.flip(kou_img,True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        kou_rct = kou_img.get_rect() #硬貨トンのレクとを抽出
+        kou_rct.center = 300,200
+        screen.blit(kou_img,kou_rct) 
         pg.display.update()
         tmr += 1        
         clock.tick(200)

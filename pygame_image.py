152a4a094417d854e9a0cb0bd58c47461eb65ff1
yujin <c0a23152cc@edu.teu.ac.jp>
2024-06-11 16:44:43 +0900

    こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -14,6 +14,8 @@ def main():
     kou_img = pg.image.load("fig/3.png")
     kou_img = pg.transform.flip(kou_img,True, False)
     tmr = 0
+    kou_rct = kou_img.get_rect() #硬貨トンのレクとを抽出
+    kou_rct.center = 300,200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -22,8 +24,15 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
-        kou_rct = kou_img.get_rect() #硬貨トンのレクとを抽出
-        kou_rct.center = 300,200
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kou_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kou_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            kou_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kou_rct.move_ip((1,0))
         screen.blit(kou_img,kou_rct) 
         pg.display.update()
         tmr += 1        

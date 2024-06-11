1a063255cabcccfdf1cbdc53fea315d1daecff97
yujin <c0a23152cc@edu.teu.ac.jp>
2024-06-11 16:31:22 +0900

    ワープ対策

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     kou_img = pg.image.load("fig/3.png")
     kou_img = pg.transform.flip(kou_img,True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x=tmr%800
+        x=tmr%3200
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
         kou_rct = kou_img.get_rect() #硬貨トンのレクとを抽出
         kou_rct.center = 300,200
         screen.blit(kou_img,kou_rct) 
